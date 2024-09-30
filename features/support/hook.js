1bfafa9afd340aadc3fadcb094f96e9a94d7477d
cardozclive <cardozclive@gmail.com>
2024-09-30 20:54:39 +0530

    now

--- features/support/hook.js
+++ features/support/hook.js
@@ -1,6 +1,6 @@
 const playwright = require('@playwright/test');
 const { POManager } = require('../../tests/PageObjects/POManager');
-const { Before, After } = require('@cucumber/cucumber');
+const { Before, After, AfterStep, Status } = require('@cucumber/cucumber');
 const { expect } = require('@playwright/test');
 
 Before(async function () {
@@ -10,6 +10,15 @@ Before(async function () {
     // this.poManager = new POManager(this.page, expect, username)
     this.poManager = new POManager(this.page, expect)
 });
+
+AfterStep(async function ({result}) {
+
+    if (result.status === Status.FAILED)
+    {
+        await this.page.screenshot({path:screenshot.png1});
+    }
+
+})
 
 After(function () {
     // Assuming this.driver is a selenium webdriver
